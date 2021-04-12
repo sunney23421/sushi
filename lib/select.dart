2e2db6f31e44d05fbb51e2cbbbfa3fc6b75eb802
Chaiwat Kasetsinchai <Doglnw8@gmail.com>
2021-04-12 18:28:18 +0700

    update sp screen

--- lib/select.dart
+++ lib/select.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:sushiProjectZero/main.dart';
+import 'package:sushiProjectZero/detection.dart';
 
 class SelectChoice extends StatefulWidget {
   SelectChoice({Key key}) : super(key: key);
